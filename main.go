ca72f8a98b45565f5bdc192de8599ce692341992
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-07-07 13:00:33 +0900

    とってくるデータ量を増やしました。コードを整理しました

--- main.go
+++ main.go
@@ -45,13 +45,7 @@ type Pokemons struct {
 //関数たち
 func showPokemon(c echo.Context) error {
 	url := "https://pokeapi.co/api/v2/pokemon"
-	url2 := "https://pokeapi.co/api/v2/pokemon?offset=20\u0026limit=20"
-	//以下他のapiから情報を持ってきてデータをjson形式に加工（もし情報を取ってくる先のurlの数が増えたら、その分だけ行数が増える）
 	res, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	res2, err := http.Get(url2)
 	if err != nil {
 		panic(err)
 	}
@@ -59,30 +53,82 @@ func showPokemon(c echo.Context) error {
 	if err != nil {
 		panic(err)
 	}
-	body2, err := ioutil.ReadAll(res2.Body)
-	if err != nil {
-		panic(err)
-	}
 	defer res.Body.Close()
-	defer res2.Body.Close()
 
-	var response, response2 Response
+	var response Response
 	var responses []Response
 
 	if err := json.Unmarshal(body, &response); err != nil {
 		panic(err)
 	}
-	if err := json.Unmarshal(body2, &response2); err != nil {
-		panic(err)
-	}
-	responses = append(responses, response, response2)
-	for i := 0; i <= 1; i++ {
+	responses = append(responses, response)
+	for i := 0; i <= 0; i++ {
 		for j := 0; j < 20; j++ {
 			pokemons = append(pokemons, Pokemons{Name: responses[i].Results[j].Name, Url: responses[i].Results[j].URL, Id: i*20 + j + 1})
 		}
 	}
+	/////////////////////////////
+	for i := 1; i < 57; i++ {
+		var response Response
+		url := "https://pokeapi.co/api/v2/pokemon?offset=" + strconv.Itoa(i*20) + "\u0026limit=" + strconv.Itoa(i*20)
+		res, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		body, err := ioutil.ReadAll(res.Body)
+		if err := json.Unmarshal(body, &response); err != nil {
+			panic(err)
+		}
+		responses = append(responses, response)
+		for j := 0; j < 20; j++ {
+			pokemons = append(pokemons, Pokemons{Name: responses[i].Results[j].Name, Url: responses[i].Results[j].URL, Id: i*20 + j + 1})
+		}
+	}
+	/////////////////////////////
+
 	return c.JSON(http.StatusOK, pokemons)
 }
+
+// func showPokemon(c echo.Context) error {
+// 	url := "https://pokeapi.co/api/v2/pokemon"
+// 	url2 := "https://pokeapi.co/api/v2/pokemon?offset=20\u0026limit=20"
+// 	//以下他のapiから情報を持ってきてデータをjson形式に加工（もし情報を取ってくる先のurlの数が増えたら、その分だけ行数が増える）
+// 	res, err := http.Get(url)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	res2, err := http.Get(url2)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	body, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	body2, err := ioutil.ReadAll(res2.Body)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer res.Body.Close()
+// 	defer res2.Body.Close()
+
+// 	var response, response2 Response
+// 	var responses []Response
+
+// 	if err := json.Unmarshal(body, &response); err != nil {
+// 		panic(err)
+// 	}
+// 	if err := json.Unmarshal(body2, &response2); err != nil {
+// 		panic(err)
+// 	}
+// 	responses = append(responses, response, response2)
+// 	for i := 0; i <= 1; i++ {
+// 		for j := 0; j < 20; j++ {
+// 			pokemons = append(pokemons, Pokemons{Name: responses[i].Results[j].Name, Url: responses[i].Results[j].URL, Id: i*20 + j + 1})
+// 		}
+// 	}
+// 	return c.JSON(http.StatusOK, pokemons)
+// }
 
 func showDatumByName(c echo.Context) error {
 	name := c.FormValue("name")
